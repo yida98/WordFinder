55eb1371960ee2fb2c561a857f640301a52d5ab6
Yida Zhang <yidazhang@frk.lan>
2022-09-21 21:44:12 -0700

    Search toolbar

---
+++ Lexiscope/Views/SearchStyleView/SearchToolbar.swift
@@ -0,0 +1,22 @@
+//
+//  SearchToolbar.swift
+//  Lexiscope
+//
+//  Created by Yida Zhang on 9/21/22.
+//
+
+import SwiftUI
+
+struct SearchToolbar: View {
+    var body: some View {
+        HStack {
+            Text("Hola World!")
+        }
+    }
+}
+
+struct SearchToolbar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchToolbar()
+    }
+}
--- Lexiscope/Views/SearchStyleView/SearchView.swift
+++ Lexiscope/Views/SearchStyleView/SearchView.swift
@@ -11,24 +11,33 @@ struct SearchView: View {
     @EnvironmentObject var viewModel: SearchViewModel
     
     var body: some View {
-        ZStack {
-            if viewModel.cameraSearch {
-                CameraViewRepresentable(viewModel: CameraViewModel())
-            } else {
-                AudioView()
+        VStack {
+            ZStack {
+                if viewModel.cameraSearch {
+                    CameraViewRepresentable(viewModel: CameraViewModel())
+                } else {
+                    AudioView()
+                }
+                 
+                HStack {
+                    Spacer()
+                    SearchStyleToggleView()
+                        .environmentObject(viewModel)
+                }
             }
-             
-            HStack {
-                Spacer()
-                SearchStyleToggleView()
-                    .environmentObject(viewModel)
+            .mask {
+                RoundedRectangle(cornerRadius: 20)
+                /// Note: Putting the shadow modifier here allows the original view to pass through from the shadow. The mask modifier applies any opacity of the masking view.
             }
+            .frame(width: Constant.screenBounds.width - 40, height: 150)
+            .clipped()
+
+            SearchToolbar()
         }
+        .background(.gray)
         .mask {
             RoundedRectangle(cornerRadius: 20)
-            /// Note: Putting the shadow modifier here allows the original view to pass through from the shadow. The mask modifier applies any opacity of the masking view.
         }
-        .frame(width: Constant.screenBounds.width - 40, height: 150)
         .clipped()
     }
 }
