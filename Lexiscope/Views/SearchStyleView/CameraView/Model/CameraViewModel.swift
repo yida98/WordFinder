07569fcee768b84c6cc80e0249e4b1e89024c71d
Yida Zhang <carriedada@gmail.com>
2022-10-14 11:41:54 -0700

    Restored Lexiscope/Views/SearchStyleView/CameraView/CameraViewModel.swift

--- Lexiscope/Views/SearchStyleView/CameraView/Model/CameraViewModel.swift
+++ Lexiscope/Views/SearchStyleView/CameraView/Model/CameraViewModel.swift
@@ -10,20 +10,14 @@ import Vision
 import Combine
 import AVFoundation
 
-class CameraViewModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampleBufferDelegate, AVCapturePhotoCaptureDelegate {
+class CameraViewModel: NSObject,
+                        ObservableObject,
+                        AVCaptureVideoDataOutputSampleBufferDelegate,
+                        AVCapturePhotoCaptureDelegate,
+                        AVCaptureVideoTextDetectionDelegate {
     
-    @Published var capturedImage: Bool = false
+    @Published var hasCapturedImage: Bool = false
     @Published var loading: Bool = false
-    
-    // MARK: Camera variables
-    @Published var coordinates: CGRect = .zero
-    @Published var bufferSize: CGSize = CGSize(width: 1, height: 1) {
-        willSet {
-            trueCameraHeight = Constant.screenBounds.width / (newValue.height / newValue.width)
-        }
-    }
-    
-    @Published var trueCameraHeight: CGFloat = 1
     
     @Published var word: String = ""
 //    @Published var headwordEntry: HeadwordEntry? {
@@ -84,70 +78,10 @@ class CameraViewModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampl
         }
     }
     
-// MARK: - Live Video
-    private let session = AVCaptureSession()
-    private let deviceOutput = AVCaptureVideoDataOutput()
-    var previewLayer: AVCaptureVideoPreviewLayer! = nil
-    private static let maxCandidates = 1 // TODO: Delegate out these customizations to others
-    static let bufferRatio: CGFloat = 640/480
-    var isRunning: Bool = false
-    
-//    var bufferSize: CGSize = .zero
-    
-    func startLiveVideo() -> AVCaptureVideoPreviewLayer? {
-        request = VNRecognizeTextRequest(completionHandler: detectText(request:error:))
-
-        // FIXME: Super slow creating the first view. Async some functions
-        session.sessionPreset = .vga640x480
-        bufferSize = CGSize(width: 640, height: 480)
-        
-        var deviceInput: AVCaptureDeviceInput!
-        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
-            debugPrint("No video device; might be using a simulator.")
-            return nil
-        }
-        
-        do {
-            deviceInput = try AVCaptureDeviceInput(device: videoDevice)
-        } catch {
-            print("Could not create video device input: \(error)")
-            return nil
-        }
-        
-//        deviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
-        
-        deviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)]
-        
-        deviceOutput.alwaysDiscardsLateVideoFrames = true
-        deviceOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global())
-        session.addInput(deviceInput)
-        session.addOutput(deviceOutput)
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        if let previewLayerConnection = previewLayer.connection {
-            previewLayerConnection.videoOrientation = .portrait
-        }
-        
-        if let deviceConnection = deviceOutput.connection(with: .video) {
-            deviceConnection.isEnabled = true
-            deviceConnection.preferredVideoStabilizationMode = .off
-        }
-        
-        return previewLayer
-    }
-    
-    func startRunning() {
-        session.startRunning()
-        isRunning = true
-    }
-    
-    func stopRunning() {
-        session.stopRunning()
-        isRunning = false
-    }
-
 // MARK: - Text Detection
-    private func detectText(request: VNRequest, error: Error?) {
+    private static let maxCandidates = 1 // TODO: Delegate out these customizations to others
+    
+    internal func detectText(request: VNRequest, error: Error?) {
         if error != nil {
             debugPrint(error.debugDescription)
             return
@@ -160,17 +94,118 @@ class CameraViewModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampl
         
         if let result = Self.closestTo(.bottom, in: results) {
             if let recognizedText = result.topCandidates(Self.maxCandidates).first {
-                var bounds = result.boundingBox
+                let bounds = Self.boundingBox(forRegionOfInterest: result.boundingBox, fromOutput: CameraViewModel.viewportSize)
                 DispatchQueue.main.async { [self] in
-                    bounds = Self.boundingBox(forRegionOfInterest: bounds, fromOutput: CameraViewModel.viewportSize)
                     coordinates = bounds
                     word = recognizedText.string
                 }
             }
         }
     }
+
+// MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
     
-// MARK: Helper Functions
+    var request: VNRecognizeTextRequest!
+    var sequenceHandler = VNSequenceRequestHandler()
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+
+
+        request.recognitionLevel = .accurate
+//            request.usesLanguageCorrection = true
+//            request.recognitionLanguages = [
+
+        /// The origin point is the lower-left corner, not the centre origin
+        let height = (Constant.screenBounds.width / (bufferSize.height / bufferSize.width))
+        let originX = (Constant.screenBounds.width - CameraViewModel.viewportSize.width) / 2
+        let originY = (height - CameraViewModel.viewportSize.height)/2
+
+        request.regionOfInterest = Self.normalizeBounds(for: CGRect(origin: CGPoint(x: originX,
+                                                                                    y: originY),
+                                                                    size: CameraViewModel.viewportSize),
+                                                        in: bufferSize)
+//            let imageRequestHandler = VNImageRequestHandler(cgImage: imageFromSampleBuffer(sampleBuffer : sampleBuffer).cgImage!, options: [:])
+//            let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:])
+        /// Turned off `Address Sanitizer` to ensure `VNImageRequestHandler` doesn't deallocate non-allocated memory
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer,
+                                                        orientation: CGImagePropertyOrientation.right,
+                                                        options: [:])
+
+        do {
+            try imageRequestHandler.perform([request])
+        } catch {
+            // TODO: Handle errors
+            debugPrint(error)
+        }
+    }
+    
+    // MARK: Camera variables
+    var camera: TextDetectionCameraModel?
+    let sessionPreset: AVCaptureSession.Preset = .vga640x480
+    @Published var capturedImage: UIImage?
+    @Published var coordinates: CGRect = .zero
+    @Published var bufferSize: CGSize = CGSize(width: 640, height: 480)
+    static let bufferRatio: CGFloat = 640/480
+    
+    // MARK: - AVCapturePhotoCaptureDelegate
+        
+    internal func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        let previewImage = UIImage(data: imageData)
+        capturedImage = previewImage
+    }
+    
+    func takePhoto() {
+        var photoSettings = AVCapturePhotoSettings()
+        if let photoPreviewType = photoSettings.availablePreviewPhotoPixelFormatTypes.first,
+            let camera = camera {
+            photoSettings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: photoPreviewType]
+            camera.capturePhoto(with: photoSettings, delegate: self)
+            
+            self.camera = nil
+        }
+    }
+    
+    func startCamera() {
+        camera = TextDetectionCameraModel(sessionPreset: sessionPreset,
+                                          captureVideoTextDetectionDelegate: self)
+        camera?.startRunning()
+    }
+    
+    func resumeCamera() {
+        capturedImage = nil
+    }
+    
+    func cameraPreviewLayer() -> CALayer? {
+        return camera?.startLiveVideo()
+    }
+    
+    // MARK: Helper funcs
+    
+    /// Calculate the `regionOfInterest` in the `bufferSize` normalized for the screen size
+    private static func normalizeBounds(for regionOfInterest: CGRect, in bufferSize: CGSize) -> CGRect {
+        
+        var rect = regionOfInterest
+        let width = Constant.screenBounds.width
+        let height = width / (bufferSize.height / bufferSize.width)
+        rect.origin = CGPoint(x: rect.minX/width, y: rect.minY/height)
+        rect.size = CGSize(width: rect.size.width/width, height: rect.size.height/height)
+        return rect
+    }
+    
+    private static func normalizeSize(for regionOfInterest: CGSize, in bufferSize: CGSize) -> CGSize {
+        
+        var size = regionOfInterest
+        let width = Constant.screenBounds.width
+        let height = width / (bufferSize.height / bufferSize.width)
+
+        size = CGSize(width: size.width/width, height: size.height/height)
+        
+        return size
+    }
+
     private static func closestTo(_ point: Point,in results: [VNRecognizedTextObservation]) -> VNRecognizedTextObservation? {
         return results.reduce(results.first) { result, observation in
             var prevDistance: Float = 0
@@ -213,7 +248,6 @@ class CameraViewModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampl
         }
     }
     
-    
     fileprivate static func boundingBox(forRegionOfInterest: CGRect, fromOutput size: CGSize) -> CGRect {
         
         let imageWidth = size.width
@@ -239,90 +273,9 @@ class CameraViewModel: NSObject, ObservableObject, AVCaptureVideoDataOutputSampl
 
         return rect
     }
-
-// MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
-    
-    var request: VNRecognizeTextRequest!
-    var sequenceHandler = VNSequenceRequestHandler()
-    
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-
-
-        request.recognitionLevel = .accurate
-//            request.usesLanguageCorrection = true
-//            request.recognitionLanguages = [
-
-        /// The origin point is the lower-left corner, not the centre origin
-        let height = (Constant.screenBounds.width / (bufferSize.height / bufferSize.width))
-        let originX = (Constant.screenBounds.width - CameraViewModel.viewportSize.width) / 2
-        let originY = (height - CameraViewModel.viewportSize.height)/2
-
-        request.regionOfInterest = Self.normalizeBounds(for: CGRect(origin: CGPoint(x: originX,
-                                                                                    y: originY),
-                                                                    size: CameraViewModel.viewportSize),
-                                                        in: bufferSize)
-//            let imageRequestHandler = VNImageRequestHandler(cgImage: imageFromSampleBuffer(sampleBuffer : sampleBuffer).cgImage!, options: [:])
-//            let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:])
-        /// Turned off `Address Sanitizer` to ensure `VNImageRequestHandler` doesn't deallocate non-allocated memory
-        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer,
-                                                        orientation: CGImagePropertyOrientation.right,
-                                                        options: [:])
-
-        do {
-            try imageRequestHandler.perform([request])
-        } catch {
-            // TODO: Handle errors
-            debugPrint(error)
-        }
-    }
-    
-    // MARK: Helper funcs
-    
-    /// Calculate the `regionOfInterest` in the `bufferSize` normalized for the screen size
-    private static func normalizeBounds(for regionOfInterest: CGRect, in bufferSize: CGSize) -> CGRect {
-        
-        var rect = regionOfInterest
-        let width = Constant.screenBounds.width
-        let height = width / (bufferSize.height / bufferSize.width)
-        rect.origin = CGPoint(x: rect.minX/width, y: rect.minY/height)
-        rect.size = CGSize(width: rect.size.width/width, height: rect.size.height/height)
-        return rect
-    }
-    
-    private static func normalizeSize(for regionOfInterest: CGSize, in bufferSize: CGSize) -> CGSize {
-        
-        var size = regionOfInterest
-        let width = Constant.screenBounds.width
-        let height = width / (bufferSize.height / bufferSize.width)
-
-        size = CGSize(width: size.width/width, height: size.height/height)
-        
-        return size
-    }
-    
-    // MARK: - AVCapturePhotoCaptureDelegate
-    
-    let photoOutput = AVCapturePhotoOutput()
-    
-    internal func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let imageData = photo.fileDataRepresentation() else { return }
-        let previewImage = UIImage(data: imageData)
-        
-//            viewModel.
-    }
-    
-    func takePhoto() {
-        let photoSettings = AVCapturePhotoSettings()
-//        if let photoPreviewType = photoSettings.availablePreviewPhotoPixelFormatTypes.first {
-//            photoSettings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: photoPreviewType]
-//            photoOutput.capturePhoto(with: photoSettings, delegate: self)
-//        }
-    }
 }
 
-extension CameraViewModel {
-//    static let scannerSize: CGSize = CGSize(width: , height: <#T##CGFloat#>)
+protocol AVCaptureVideoTextDetectionDelegate {
+    func detectText(request: VNRequest, error: Error?)
 }
 	
