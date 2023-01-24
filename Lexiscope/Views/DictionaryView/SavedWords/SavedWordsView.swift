08cbf88f7c073356fbf49daa69cbfcc8739cca56
Yida Zhang <carriedada@gmail.com>
2023-01-24 15:22:10 -0800

    wfdr://cf47c7a7b4384353865bcd0c94e0e7b1 Alphabetical order

--- Lexiscope/Views/DictionaryView/SavedWords/SavedWordsView.swift
+++ Lexiscope/Views/DictionaryView/SavedWords/SavedWordsView.swift
@@ -17,17 +17,58 @@ struct SavedWordsView: View {
                     Image(systemName: "text.book.closed")
                         .opacity(0.5)
                 } else {
-                    ForEach(viewModel.vocabulary!) { vocabularyEntry in
-                        DefinitionView(viewModel: DefinitionViewModel(vocabularyEntry: vocabularyEntry),
-                                       expanded: $viewModel.expanded)
-                        .id(vocabularyEntry.word)
-                        .onTapGesture(perform: {
-                            handleTap(on: vocabularyEntry, scrollProxy: reader)
-                        })
+                    ForEach(alphabetizedKeys(), id: \.self) { key in
+                        Section {
+                            ForEach(alphabetizedDictionary()[key]!) { entry in
+                                DefinitionView(viewModel: DefinitionViewModel(vocabularyEntry: entry),
+                                               expanded: $viewModel.expanded)
+                                .id(entry.word)
+                                .onTapGesture(perform: {
+                                    handleTap(on: entry, scrollProxy: reader)
+                                })
+                            }
+                        } header: {
+                            HStack {
+                                Text("\(key.uppercased())")
+                                Spacer()
+                            }
+                        }
+                        
                     }
                 }
             }
         }
+    }
+    
+    private func alphabetizedDictionary() -> [Character: [VocabularyEntry]] {
+        guard let vocabulary = viewModel.vocabulary else { return [:] }
+        
+        var dict = [Character: [VocabularyEntry]]()
+        for vocabularyEntry in vocabulary {
+            let firstLetter = vocabularyEntry.word!.first!
+            if dict.keys.contains(firstLetter) {
+                var array = dict[firstLetter]
+                array!.append(vocabularyEntry)
+                array!.sort(by: { $0.word! < $1.word! })
+                dict[firstLetter] = array
+            } else {
+                dict[firstLetter] = [vocabularyEntry]
+            }
+        }
+        
+        return dict
+    }
+    
+    private func alphabetizedKeys() -> [Character] {
+        return alphabetizedDictionary().keys.sorted()
+    }
+    
+    private func alphabetizedVocabulary() -> [[VocabularyEntry]] {
+        var result = [[VocabularyEntry]]()
+        for key in alphabetizedKeys() {
+            result.append(alphabetizedDictionary()[key]!)
+        }
+        return result
     }
     
     private func handleTap(on vocabularyEntry: VocabularyEntry, scrollProxy: ScrollViewProxy) {
