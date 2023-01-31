fd813386f0a85f4f4ca4a4e1ecbda2f71a1f341c
Yida Zhang <carriedada@gmail.com>
2023-01-30 23:15:46 -0800

    wfdr://ef79ce69da3c45deb60b7cd02a6f4490 Neumorphism test

--- Lexiscope/Views/DictionaryView/Quiz/QuizOptionCell.swift
+++ Lexiscope/Views/DictionaryView/Quiz/QuizOptionCell.swift
@@ -11,11 +11,16 @@ struct QuizOptionCell: View {
     var text: String
     var id: Int
     @Binding var choice: Int?
+    var parentGeometryProxy: GeometryProxy
+    
     var body: some View {
-        Button {
-            choice = id
-        } label: {
+        let binding = Binding<Bool>(get: { self.id == self.choice }, set: { _ in self.choice = self.id })
+        Toggle(isOn: binding) {
             Text(text)
+                .foregroundColor(.white)
         }
+        .frame(maxWidth: parentGeometryProxy.size.width)
+        .padding()
+        .toggleStyle(NeumorphicToggleStyle(shape: RoundedRectangle(cornerRadius: 30), start: .periwinkleCrayola, end: .boyBlue))
     }
 }
--- Lexiscope/Views/DictionaryView/Quiz/QuizView.swift
+++ Lexiscope/Views/DictionaryView/Quiz/QuizView.swift
@@ -8,22 +8,23 @@
 import SwiftUI
 
 struct QuizView: View {
-    
-    @StateObject var viewModel = QuizViewModel()
+    @StateObject var viewModel = QuizViewModel() // TODO: @ObservedObject var viewModel: QuizViewModel
     @State var choice: Int?
     
     var body: some View {
         if let question = viewModel.question {
             VStack {
                 Text("\(question.text)")
-                VStack {
-                    HStack {
-                        QuizOptionCell(text: viewModel.option(0, for: question), id: 0, choice: $choice)
-                        QuizOptionCell(text: viewModel.option(1, for: question), id: 1, choice: $choice)
-                    }
-                    HStack {
-                        QuizOptionCell(text: viewModel.option(2, for: question), id: 2, choice: $choice)
-                        QuizOptionCell(text: viewModel.option(3, for: question), id: 3, choice: $choice)
+                GeometryReader { proxy in
+                    VStack {
+                        HStack {
+                            QuizOptionCell(text: viewModel.option(0, for: question), id: 0, choice: $choice, parentGeometryProxy: proxy)
+                            QuizOptionCell(text: viewModel.option(1, for: question), id: 1, choice: $choice, parentGeometryProxy: proxy)
+                        }
+                        HStack {
+                            QuizOptionCell(text: viewModel.option(2, for: question), id: 2, choice: $choice, parentGeometryProxy: proxy)
+                            QuizOptionCell(text: viewModel.option(3, for: question), id: 3, choice: $choice, parentGeometryProxy: proxy)
+                        }
                     }
                 }
                 Button {
@@ -37,7 +38,7 @@ struct QuizView: View {
                 } label: {
                     Text("Submit")
                 }
-            }
+            }.background(.white)
         } else {
             Text("placeholder view")
         }
